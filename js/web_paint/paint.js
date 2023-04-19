1ec250be4e483a579b0a987fe4760aa4ee35253f
lionel <staricofflionel@hotmail.com>
2023-04-19 12:50:35 -0300

    se agrega la carpeta js y se agrega el proyecto paint

---
+++ js/web_paint/paint.js
@@ -0,0 +1,47 @@
+let pantalla = document.querySelector("canvas");
+let pincel = pantalla.getContext("2d");
+
+pincel.fillStyle = "grey";
+pincel.fillRect(0,0,600,400);
+
+
+//evento: se refiere al mouse cuando hace click
+//esta funcion indica en que jugar de la pantalla hace click el mouse
+function exibirAlerta(evento){
+    let x = evento.pageX - pantalla.offsetLeft; //resta de la pantalla lo que no pertenece al canvas del lado izquierdo como el margin
+    let y = evento.pageY - pantalla.offsetTop;
+    alert(x + ", " + y);
+}
+
+
+function dibujarCirculo(evento){
+    let x = evento.pageX - pantalla.offsetLeft;
+    let y = evento.pageY - pantalla.offsetTop;
+
+//funcionalidad para cambiar de color
+let color = document.querySelector("#color").value;
+
+pincel.fillStyle= color;
+
+  //  pincel.fillStyle = "blue";
+    pincel.beginPath();
+    pincel.arc(x,y,10,0,2*Math.PI);
+    pincel.fill();
+   // console.log(x + ", "+y);
+
+}
+
+function borrarPantalla(){
+    pincel.fillStyle = "grey";
+    pincel.fillRect(0,0,600,400);
+
+}
+
+
+
+//fincionalidad del boton borrar
+let btnBorrar = document.querySelector("#borrar");
+btnBorrar.onclick = borrarPantalla;
+
+//funcionalidad para dibujar circulos en pantalla
+pantalla.onclick = dibujarCirculo;
