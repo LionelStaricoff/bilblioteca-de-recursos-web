7b853f0e34656a39da693b77554bc5a084ee7302
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2024-02-24 17:35:30 -0300

    mejora en el menú responsive de la sección Librería

--- js/menu.js
+++ js/menu.js
@@ -10,22 +10,8 @@ enlacesMenu.forEach(enlace => {
 });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const checkBtn = document.getElementById("check-2");
-    const menuMobile = document.querySelector(".menuMobile");
 
-    checkBtn.addEventListener("change", function () {
-        menuMobile.style.transform = checkBtn.checked ? "translateX(0)" : "translateX(100%)";
-    });
 
-    // Cierra el menú si se hace clic en un enlace del menú
-    const menuLinks = document.querySelectorAll(".menuMobile .listado a");
-    menuLinks.forEach(function (link) {
-        link.addEventListener("click", function () {
-            checkBtn.checked = false;
-            menuMobile.style.transform = "translateX(100%)";
-        });
-    });
-});
+
 
 
