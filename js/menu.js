23bc8633b0ef1bcb3562abefac8af2a6ddfc5f47
lionel staricoff <staricofflionel@hotmail.com>
2024-02-29 17:26:20 -0500

    se agrega slider manual1

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-
+import * as obj from './main';
 
 // Obtener todos los enlaces dentro del menú
 const enlacesMenu = document.querySelectorAll('nav ul li a');
@@ -12,18 +12,7 @@ enlacesMenu.forEach(enlace => {
 });
 
 
-/*
-    let checkbox = document.getElementById('check-2');
-        checkbox.addEventListener('change', function () {
-            let menuListado = document.querySelector('.listado');
-            let menuMobile = document.querySelector('.menuMobile');
 
-            if (menuListado) {
-                menuListado.style.display = checkbox.checked ? 'block' : 'none';
-                menuMobile.style.display = checkbox.checked ? 'block' : 'none';
-
-            }
-        });*/
     
 
 
