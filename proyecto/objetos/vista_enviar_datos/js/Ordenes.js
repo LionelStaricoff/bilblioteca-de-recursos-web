06fac817cbcadfd8b35a9b4504312327b7390aeb
lionel staricoff <staricofflionel@hotmail.com>
2024-03-02 22:02:49 -0500

    creando carpeta objetos para colocarlos, se agrega un objeto para mandar datos

---
+++ proyecto/objetos/vista_enviar_datos/js/Ordenes.js
@@ -0,0 +1,38 @@
+export class Ordenes {
+    constructor(dato, divPadre) {
+        this.datos = dato;
+        this.textarea;
+        this.divPadre = divPadre ?? '#front';
+        this.orden = {
+            "description": this.textarea ?? "nueva orden test",
+            "cliente_id": 15
+        }
+    }
+
+    crearOrden() {
+        const div = document.createElement('div');
+        div.id = "CrearOrden";
+
+        const textarea = document.createElement('textarea');
+        textarea.name = "descripcion";
+        textarea.placeholder = "orden";
+        textarea.cols = "30";
+        textarea.rows = "8";
+        textarea.innerText = this.textarea ?? "";
+
+        const button = document.createElement('button');
+        button.innerText = "Crear";
+        button.addEventListener('click', () => {
+             console.log(this.datos);
+
+            this.orden.description = document.querySelector('textarea').value;
+            this.textarea = console.log(this.orden);
+        })
+        div.append(textarea, button);
+        return div;
+    }
+    agregarAlFront() {
+        const main = document.querySelector(this.divPadre);
+        main.appendChild(this.crearOrden());
+    }
+}
