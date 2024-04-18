27ba3196629a98288f5c6ce2d1852320e3f0de22
lionel staricoff <staricofflionel@hotmail.com>
2024-04-18 08:04:04 -0500

    agregando las letras blancas

--- js/utils.js
+++ js/utils.js
@@ -43,6 +43,7 @@ export class ColorearHtml {
         codigoHtml.style.backgroundColor = "#333333";
         codigoHtml.style.padding = '1em';
         codigoHtml.style.textAlign  = 'center';
+        codigoHtml.style.color  = 'gainsboro';
  
         const contenido = codigoHtml.innerHTML;
 
