18ac58d03866f6859c18c03c2a65eb5817945ea3
Luma2001 <lumaqui2001@yahoo.com.ar>
2024-04-30 16:13:10 -0300

    se crea objeto de libreria

--- js/index.js
+++ js/index.js
@@ -1,82 +1,246 @@
+import { librerias } from './libreria.js';
+// import { CartelError } from './utils.js';
+
+
+const iframe = document.querySelector('iframe[name="codeFrame"]');
+iframe.style.width="100%";
+iframe.style.height="70vh";
+iframe.style.margin="20px";
+
+
+const verEjemplo= (enlace,html,css,js,obj)=>{
+    
+    console.log("Mostrando enlace: "+ enlace);
+    console.log("Mostrando html: "+ html);
+    console.log("Mostrando css: "+ css);
+    console.log("Mostrando js: "+ js);
+    console.log("Mostrando obj: "+ obj);
+
+
+    const ej = document.querySelector(".ejemplo");
+    const btnvista= document.getElementById("btnVista");
+    const btnhtml = document.getElementById("btnHtml");
+    const btncss = document.getElementById("btnCss");
+    const btnjs = document.getElementById("btnJs");
+    const btnobj = document.getElementById("btnObject");
+
+    btnvista.addEventListener('click',()=>{iframe.src=enlace;});
+    btnhtml.addEventListener('click',()=>{ej.innerText= html;});
+    btncss.addEventListener('click',()=>{ej.innerHTML=css;});
+    btnjs.addEventListener('click',()=>{ej.innerHTML=js;});
+    btnobj.addEventListener('click',()=>{ej.innerHTML=obj;});
+   
+}
+
+
+function mostrarObjeto(objeto) {
+    
+    const container = document.querySelector('.listado');
+    const details = document.createElement('details');
+    const summary = document.createElement('summary');
+    summary.textContent = objeto.titulo;
+    details.appendChild(summary);
+
+    const ejemplos = objeto.ejemplos;
+
+    ejemplos.forEach(ejemplo => {
+                     
+                    const link = document.createElement('a');
+                    link.textContent = ejemplo.titulo;
+                    link.href = ejemplo.href;
+                    link.target= "codeFrame";
+
+                    const paragraph = document.createElement('p');
+                    paragraph.appendChild(link);
+                    paragraph.addEventListener('click',()=>{verEjemplo(ejemplo.href,ejemplo.html,ejemplo.css,ejemplo.js,ejemplo.objeto)});
+                    details.appendChild(paragraph);
+    })
+    container.appendChild(details);
+}
+
+function getAllObjects(){
+
+    librerias.forEach(libreria => {
+        mostrarObjeto(libreria);
+    });
+
+}
 
 
 
 
-// const btncopiar =document.querySelector(".iconoCopy");
-// const iframe = document.querySelector('iframe[name="codeFrame"]');
-// iframe.style.background= "#1e1d1d";
-// iframe.style.height="80vh";
-// iframe.style.margin="20px"
 
-// iframe.contentDocument || || iframe.contentWindow.document;
-//código para copiar texto en portapapeles
-// function copiarTexto(){
-//     const iframeDoc = iframe.contentWindow.document;
-//     const codigo = iframeDoc.querySelector('pre code').innerText;
-//     if (codigo) {
-//         navigator.clipboard.writeText(codigo)
-//           .then(function () {
-//             alert("Código copiado al portapapeles");
-//           })
-//           .catch(function () {
-//             alert("Error al copiar el código");
-//           });
-//       } else {
-//         alert("Error. No hay código para copiar.");
-//       }
-// }
+getAllObjects();
 
 
-// const verEjemplo= (enlace,vista,html,css,js,obj)=>{
-//     iframe.src = `${enlace}${vista}`;
-//     const btnvista= document.getElementById("vista");
-//     const btnhtml = document.getElementById("html");
-//     const btncss = document.getElementById("css");
-//     const btnjs = document.getElementById("js");
-//     const btnobj = document.getElementById("object");
 
-//     btnvista.addEventListener('click',()=>{iframe.src = `${enlace}${vista}`});
-//     btnhtml.addEventListener('click',()=>{iframe.src = `${enlace}${html}`;});
-//     btncss.addEventListener('click',()=>{iframe.src = `${enlace}${css}`;});
-//     btnjs.addEventListener('click',()=>{iframe.src = `${enlace}${js}`;});
-//     btnobj.addEventListener('click',()=>{iframe.src = `${enlace}${obj}`;});
+// document.addEventListener("DOMContentLoaded", function () {
+//     const cartelError = new CartelError('.listado');
+
+//     fetch('json/menuLibreria.json')
+//         .then(response => response.json())
+//         .then(data => {
+//             const menuItems = data.menu;
+//             const listado = document.querySelector('.listado');
+
+//             menuItems.forEach(item => {
+//                 const details = document.createElement('details');
+//                 const summary = document.createElement('summary');
+//                 summary.textContent = item.titulo;
+//                 details.appendChild(summary);
+
+//                 const subitems = item.subitems;
+//                 subitems.forEach(subitem => {
+//                     const link = document.createElement('a');
+//                     link.textContent = subitem.titulo;
+//                     link.href = subitem.href;
+//                     link.target= "codeFrame";
+
+//                     const paragraph = document.createElement('p');
+//                     paragraph.appendChild(link);
+//                     paragraph.addEventListener('click',()=>{verEjemplo(subitem.href,subitem.html,subitem.css,subitem.js,subitem.object)})
+//                     details.appendChild(paragraph);
+//                 });
+
+//                 listado.appendChild(details);
+//             });
+//         })
+//         .catch(error => {
+//             cartelError.displayError(error);
+
+//         });
+// });
+
+
+
+
+
+
+// const verEjemplo= (enlace,html,css,js,obj)=>{
+//     const ej = document.querySelector(".ejemplo");
+//     const btnvista= document.getElementById("btnVista");
+//     const btnhtml = document.getElementById("btnHtml");
+//     const btncss = document.getElementById("btnCss");
+//     const btnjs = document.getElementById("btnJs");
+//     const btnobj = document.getElementById("btnObject");
+   
+//     const contHtml = "";
+//     const contCss = "";
+//     const contJs = "";
+//     const contObj = "";
+    
+//     contHtml = `${html}`;
+
+//     btnvista.addEventListener('click',()=>{iframe.src=`${enlace}`;});
+//     btnhtml.addEventListener('click',()=>{ej.appendChild(`${html}`);});
+//     btncss.addEventListener('click',()=>{ej.appendChild(css);});
+//     btnjs.addEventListener('click',()=>{ej.appendChild(js);});
+//     btnobj.addEventListener('click',()=>{ej.appendChild(obj);});
    
 // }
 
 
 
-const cargarlibreria = ()=>{
 
-    try {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const btncopiar =document.querySelector(".iconoCopy");
+// // const iframe = document.querySelector('iframe[name="codeFrame"]');
+// // iframe.style.background= "#1e1d1d";
+// // iframe.style.width="80vh";
+// // iframe.style.height="80vh";
+// // iframe.style.margin="20px"
+
+// // iframe.contentDocument || || iframe.contentWindow.document;
+// //código para copiar texto en portapapeles
+// // function copiarTexto(){
+// //     const iframeDoc = iframe.contentWindow.document;
+// //     const codigo = iframeDoc.querySelector('pre code').innerText;
+// //     if (codigo) {
+// //         navigator.clipboard.writeText(codigo)
+// //           .then(function () {
+// //             alert("Código copiado al portapapeles");
+// //           })
+// //           .catch(function () {
+// //             alert("Error al copiar el código");
+// //           });
+// //       } else {
+// //         alert("Error. No hay código para copiar.");
+// //       }
+// // }
+
+
+// // const verEjemplo= (enlace,vista,html,css,js,obj)=>{
+// //     iframe.src = `${enlace}${vista}`;
+// //     const btnvista= document.getElementById("vista");
+// //     const btnhtml = document.getElementById("html");
+// //     const btncss = document.getElementById("css");
+// //     const btnjs = document.getElementById("js");
+// //     const btnobj = document.getElementById("object");
+
+// //     btnvista.addEventListener('click',()=>{iframe.src = `${enlace}${vista}`});
+// //     btnhtml.addEventListener('click',()=>{iframe.src = `${enlace}${html}`;});
+// //     btncss.addEventListener('click',()=>{iframe.src = `${enlace}${css}`;});
+// //     btnjs.addEventListener('click',()=>{iframe.src = `${enlace}${js}`;});
+// //     btnobj.addEventListener('click',()=>{iframe.src = `${enlace}${obj}`;});
+   
+// // }
+
+
+
+// const cargarlibreria = ()=>{
+
+//     try {
         
-        fetch('./libreria.json')
-            .then(response=>response.json())
-            .then(data => {
+//         fetch('./libreria.json')
+//             .then(response=>response.json())
+//             .then(data => {
               
-                const datos = data.libreria;
-                datos.forEach(item => {
-                    const details = document.createElement("details");
-                    const summary = document.createElement("summary");
-                    summary.textContent = item.titulo;
-                    details.appendChild(summary);
-                    const ejemplos = item.ejemplos;
-                        ejemplos.forEach(ejemplo =>{
-                        const p = document.createElement("p");
-                        const ahref = document.createElement("a");
-                        ahref.href = "#";
-                        ahref.target = "codeFrame";
-                        ahref.textContent = ejemplo.nombre;
-                        p.appendChild(ahref);  
-                        // p.addEventListener('click', ()=>verEjemplo(ejemplo.enlace, ejemplo.vista, ejemplo.html,ejemplo.css,ejemplo.js,ejemplo.object))
-                        details.appendChild(p);  
-                        })
-                    document.querySelector(".listado").appendChild(details);    
-                })
-            })     
-    } catch (error) {
-        console.log(error);
-    }
+//                 const datos = data.libreria;
+//                 datos.forEach(item => {
+//                     const details = document.createElement("details");
+//                     const summary = document.createElement("summary");
+//                     summary.textContent = item.titulo;
+//                     details.appendChild(summary);
+//                     const ejemplos = item.ejemplos;
+//                         ejemplos.forEach(ejemplo =>{
+//                         const p = document.createElement("p");
+//                         const ahref = document.createElement("a");
+//                         ahref.href = "#";
+//                         ahref.target = "codeFrame";
+//                         ahref.textContent = ejemplo.nombre;
+//                         p.appendChild(ahref);  
+//                         // p.addEventListener('click', ()=>verEjemplo(ejemplo.enlace, ejemplo.vista, ejemplo.html,ejemplo.css,ejemplo.js,ejemplo.object))
+//                         details.appendChild(p);  
+//                         })
+//                     document.querySelector(".listado").appendChild(details);    
+//                 })
+//             })     
+//     } catch (error) {
+//         console.log(error);
+//     }
 
-}
+// }
 
-cargarlibreria();
+// cargarlibreria();
--- js/libreria.js
+++ js/libreria.js
@@ -1,37 +1,332 @@
+export const librerias = [
+    {
+        titulo: "Loader",
+        ejemplos:[
+            {
+                href:"../proyecto/loader/loader02/index.html",
+                titulo:"Ejemplo 1",
+                html:`
+                    
 
-export class Libreria {
-    constructor(titulo,ejemplos,nombre,enlace,object,html,css,js,object){
-        this.titulo=titulo;
-        this.ejemplos=ejemplos;
-        this.nombre=nombre;
-        this.enlace=enlace;
-        this.vista=vista;
-        this.html=html;
-        this.css=css;
-        this.js=js;
-        this.object=object;
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Loader sin setTimeOut</title>
+                    <link rel="stylesheet" href="./css/index.css">
+                    <script src="./js/loader.js"></script>
+                    <script src="https://kit.fontawesome.com/2d9fe71672.js" crossorigin="anonymous"></script>
+                </head>
+                <body>
+                
+                    <div class="loader-content">
+                        <div class="loader"></div>
+                    </div>
+                
+                    <div class="img-wrapper">
+                        <img class="img-content" src ="https://source.unsplash.com/random">
+                    </div>
+                </body>
+                </html>
+                    
+                `,
+                css: `<pre><code>
+                body{
+                    position: relative;
+                }
+                
+                .img-wrapper{
+                    height: 100vh;
+                    display: none;
+                    justify-content: center;
+                    align-items: center;
+                
+                    .img-content{
+                        width: 500px;
+                        height: auto;
+                    }
+                
+                
+                }/*fin img-wrapper*/
 
+                .loader-content{
+            
+                    position: absolute;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    width: 100vw;
+                }
+                
+                .loader {
+                    
+                    width: 80px;
+                    height: 80px;
+                    border: solid 5px #436ff3;
+                    border-top: solid 5px #e7ecf8;
+                    border-radius: 50%;
+                
+                    animation: spin 1.2s
+                                linear infinite;
+                
+                    
+                }
+                
+                @keyframes spin {
+                    to {
+                        transform: rotate(360deg);
+                    }
+                    
+                }
+                </code></pre>`,
+                js: `<pre><code>
+
+                window.onload = () => {
+                    let contenido = document.querySelector(".img-wrapper");
+                    let loading = document.querySelector(".loader");
+                    loading.style.display = 'none';
+                    contenido.style.display ='flex'; 
+                }
+                </code></pre>`,
+                objeto: `<pre><code></code></pre>`
+            // },
+            // {
+            //     href:"",
+            //     titulo:"",
+            //     html:`<pre><code></code></pre>`,
+            //     css: `<pre><code></code></pre>`,
+            //     js: `<pre><code></code></pre>`,
+            //     objeto: `<pre><code></code></pre>`
+            }
+        ]
+
+    },
+
+    {
+        titulo: "Animacion",
+        ejemplos:[
+            {
+                href:"./proyecto/loader/loader02/index.html",
+                titulo:"Ejemplo 1",
+                html:`
+                    
+
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Loader sin setTimeOut</title>
+                    <link rel="stylesheet" href="./css/index.css">
+                    <script src="./js/loader.js"></script>
+                    <script src="https://kit.fontawesome.com/2d9fe71672.js" crossorigin="anonymous"></script>
+                </head>
+                <body>
+                
+                    <div class="loader-content">
+                        <div class="loader"></div>
+                    </div>
+                
+                    <div class="img-wrapper">
+                        <img class="img-content" src ="https://source.unsplash.com/random">
+                    </div>
+                </body>
+                </html>
+                    
+                `,
+                css: `<pre><code>
+                body{
+                    position: relative;
+                }
+                
+                .img-wrapper{
+                    height: 100vh;
+                    display: none;
+                    justify-content: center;
+                    align-items: center;
+                
+                    .img-content{
+                        width: 500px;
+                        height: auto;
+                    }
+                
+                
+                }/*fin img-wrapper*/
+    
+                .loader-content{
+            
+                    position: absolute;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    width: 100vw;
+                }
+                
+                .loader {
+                    
+                    width: 80px;
+                    height: 80px;
+                    border: solid 5px #436ff3;
+                    border-top: solid 5px #e7ecf8;
+                    border-radius: 50%;
+                
+                    animation: spin 1.2s
+                                linear infinite;
+                
+                    
+                }
+                
+                @keyframes spin {
+                    to {
+                        transform: rotate(360deg);
+                    }
+                    
+                }
+                </code></pre>`,
+                js: `<pre><code>
+    
+                window.onload = () => {
+                    let contenido = document.querySelector(".img-wrapper");
+                    let loading = document.querySelector(".loader");
+                    loading.style.display = 'none';
+                    contenido.style.display ='flex'; 
+                }
+                </code></pre>`,
+                objeto: `<pre><code></code></pre>`
+            // },
+            // {
+            //     href:"",
+            //     titulo:"",
+            //     html:`<pre><code></code></pre>`,
+            //     css: `<pre><code></code></pre>`,
+            //     js: `<pre><code></code></pre>`,
+            //     objeto: `<pre><code></code></pre>`
+            }
+        ]
+    
     }
 
-    crearSummary(){
-        const details = document.createElement("details");
-        const summary = document.createElement('summary');
-        summary.textContent = this.titulo;
-        details.appendChild(summary);
-    }
+];
+// Ahora tienes el objeto listo para ser copiado por el usuario
+console.log(librerias);
 
-    crearEjemplos(){
-        const p = document.createElement("p");
-        const ahref = document.createElement("a");
-        ahref.href = "#";
-        ahref.target = "codeFrame";
-        ahref.textContent = this.nombre;
-        p.appendChild(ahref);  
-        p.addEventListener('click', ()=>verEjemplo(this.enlace, this.vista, this.html,this.css,this.js,this.object))
-        details.appendChild(p);  
-    }
+
+// const ejemploGaleria = {
+//     html: `<!DOCTYPE html>
+//   <html lang="es">
+//   <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <link rel="stylesheet" href="styles.css">
+//       <title>galeria-01</title>
+//   </head>
+//   <body>
+//       <div>
+//           <img src="img1.jpg" alt="">
+//           <img src="img2.jpg" alt="">
+//           <img src="img3.jpg" alt="">
+//           <img src="img4.jpg" alt="">
+//       </div>
+//   </body>
+//   </html>`,
+//     css: `* {
+//       margin: 0;
+//       padding: 0;
+//       box-sizing: border-box;
+//   }
+//   div {
+//       display: flex;
+//   }
+//   div img {
+//       filter: saturate(50%);
+//       width: 10%;
+//       border: 2px solid white;
+//       min-height: 90vh;
+//       height: 100%;
+//       object-fit: cover;
+//       border-radius: 12px;
+//       cursor: pointer;
+//   }
+//   div img:hover {
+//       filter: saturate(100%);
+//       width: 100%;
+//       transition: all ease 2s;
+//       max-height: 90vh;
+//   }`,
+//     js: ``,
+//     imagenes: ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"]
+//   };
+  
+//   // Ahora tienes el objeto listo para ser copiado por el usuario
+//   console.log(ejemploGaleria);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // export class Libreria {
+// //     constructor(titulo,ejemplos,nombre,enlace,object,html,css,js,object){
+// //         this.titulo=titulo;
+// //         this.ejemplos=ejemplos;
+// //         this.nombre=nombre;
+// //         this.enlace=enlace;
+// //         this.vista=vista;
+// //         this.html=html;
+// //         this.css=css;
+// //         this.js=js;
+// //         this.object=object;
+
+// //     }
+
+// //     crearSummary(){
+// //         const details = document.createElement("details");
+// //         const summary = document.createElement('summary');
+// //         summary.textContent = this.titulo;
+// //         details.appendChild(summary);
+// //     }
+
+// //     crearEjemplos(){
+// //         const p = document.createElement("p");
+// //         const ahref = document.createElement("a");
+// //         ahref.href = "#";
+// //         ahref.target = "codeFrame";
+// //         ahref.textContent = this.nombre;
+// //         p.appendChild(ahref);  
+// //         p.addEventListener('click', ()=>verEjemplo(this.enlace, this.vista, this.html,this.css,this.js,this.object))
+// //         details.appendChild(p);  
+// //     }
 
     
-}
+// // }
 
 
