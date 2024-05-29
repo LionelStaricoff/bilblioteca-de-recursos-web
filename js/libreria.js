1ec406f51fa6d1f90b3ba8405ff82056a61453ab
Luma2001 <lumaqui2001@yahoo.com.ar>
2024-05-29 12:44:43 -0300

    Se agrega dos ejemplos más de sliders

--- js/libreria.js
+++ js/libreria.js
@@ -353,7 +353,7 @@ window.onload = () => {
         ejemplos:[
             {
              
-                titulo:"Ejemplo1",
+                titulo:"Ejemplo1: Manual",
                 href:()=> new objs.slider1(".ejemplo"),
                 html:`
                 <div class="slider-wrap">
@@ -540,6 +540,360 @@ backbtn.addEventListener("click",() => {
                     ejemplo3:colocar cualquier elemento html
                     new slider1(div) ;
                 </code></pre>`
+            },
+            {
+                titulo:"Ejemplo2: Continuo Infinito",
+                href:()=> new objs.slider2(".ejemplo"),
+                html:`
+                <div class="contenedor">
+        <div class="img-slide">
+           <!--Acá se va a agregar la galería de imágenes-->
+        </div>
+    </div>
+                `,
+                css: `<pre class="codigo">
+                
+/*genero una transformación que se traslada horizontalmente, sobre el eje X. de izq a derecha*/
+@keyframes slide {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(-100%);
+    }
+  }
+
+.contenedor {
+    overflow: hidden;
+    padding:30px 0;
+    background: white; 
+    white-space: nowrap;
+    position: relative;
+
+    /*pausa la animación cuando el cursor esta encima del logo*/
+    &:hover .img-slide {
+        animation-play-state: paused;
+    }
+
+    /*Un detalle al comienzo y al final del slide*/
+    &::before, &::after {
+        position: absolute;
+        top: 0;
+        width: 200px;
+        height: 100%;
+        content: "";
+        z-index: 2;
+    }  
+    &:before {
+        left: 0%;
+        background: linear-gradient(to left,rgba(255,255,255,0),white);
+    }
+    
+    &:after {
+        right: 0%;
+        background: linear-gradient(to right,rgba(255,255,255,0),white);
+    }
+
+    /*Aplico transformación creada en linea 12 a una animación*/  
+    .img-slide {
+        display: inline-block;
+        animation: 35s slide infinite linear; /*acá puedo modificar la velocidad del slider*/
+
+        
+        /*Igualo el alto de todas las imágenes y el margen entre ellas*/
+        img {
+            height: 250px;
+            margin: 0 20px;
+        }
+
+    }/*fin estilo .img-slider*/
+    
+} /*fin estilo .contenedor*/
+
+
+                <code></code></pre>`,
+                js: `<pre class="codigo"><code>
+                /*clona galería de imágenes, para simular un efecto continuo e infinito*/
+        const copy = document.querySelector('.img-slide').cloneNode(true);
+        document.querySelector('.contenedor').appendChild(copy);
+                </code></pre>`,
+                objeto: `<pre class="codigo">
+                <pre class="codigo"><code>
+                import {slider2} from './main.js';
+                    
+                    ejemplo1: colocar un id
+                    new slider2(#id_del_padre) ;
+                        
+                    ejemplo2: colocar una clase
+                    new slider2(.class_del_padre) ;
+                        
+                    ejemplo3:colocar cualquier elemento html
+                    new slider2(div) ;
+                </code></pre>`
+
+            },
+            {
+                titulo:"Ejemplo3: Manual y automático",
+                href:()=> new objs.slider3(".ejemplo"),
+                html:`
+                <div class="contenedor">
+                <i id="left" class="fa-solid fa-angle-left"> </i>
+        
+                <ul class="carousel">
+                   <!-- acá galeria
+                        <li class="card">
+                            <div class="img"><img src="./img/Abadango Cluster Princess.jpeg" alt="Img of Abadango Cluster Princess" draggable="false"/></div>
+                            <h2>Abadango Cluster</h2>
+                            <span>ALIEN</span>
+                        </li> 
+                    -->
+                </ul>
+                <i id="right" class="fa-solid fa-angle-right"> </i>  
+            </div>
+                `,
+                css: `<pre class="codigo">
+                
+.contenedor {
+    max-width: 1100px;
+    width: 100%;
+    position: relative;
+    
+    i {
+        height: 50px;
+        width: 50px;
+        background: lightgoldenrodyellow;
+        text-align: center;
+        line-height: 50px;
+        border-radius: 50%;
+        cursor: pointer;
+        position: absolute;
+        top: 50%;
+        font-size: 1.25rem;
+        transform: translateY(-50%);
+        box-shadow: 0 3px 6px rgba(0,0,0,0.23);
+        transition: transform 0.1s linear;
+        z-index: 10;
+
+        &:active {
+            transform: translateY(-50%) scale(0.85);
+          }
+          
+        &:first-child {
+            left: -22px;
+        }
+        
+        &:last-child {
+            right: -22px;
+        }
+    }/*fin estilo i*/
+
+
+    .carousel {
+        display: grid;
+        grid-auto-flow: column;
+        grid-auto-columns: calc((100% / 3) - 12px);
+        gap: 16px;
+        overflow-x: auto;
+        scroll-snap-type: x mandatory;
+        scroll-behavior: smooth;
+        scrollbar-width: none;/*para que no se vea el scroller*/
+        border-radius: 8px;
+        
+        &::-webkit-scrollbar {
+            display: none;/*no compatible con firefox*/
+          }
+        .no-transition {
+            scroll-behavior: auto;
+          }
+
+        .dragging {
+            scroll-snap-type: none;
+            scroll-behavior: auto;
+            .card {
+            cursor: grab;
+            user-select: none;
+            }
+        }
+
+             
+        :where(.card,.img){
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+               
+        .card {
+            scroll-snap-align: start;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            height: 350px;
+            list-style: none;
+            background: whitesmoke;
+            padding-bottom: 15px;
+            border-radius: 8px;
+            cursor: pointer;
+
+            
+            .img {
+                background: #343434;
+                width: 206px;
+                height: 206px;
+                border-radius: 50%;
+
+                img {
+                    width: 200px;
+                    height: 200px;
+                    border-radius: 50%;
+                    border: 4px solid lightgoldenrodyellow;
+                    object-fit: cover;    
+                }
+                
+            }/*fin estilo .img*/
+
+            h2 {
+                font-weight:bolder;
+                font-size: 1.56rem;
+                margin: 30px 0 5px;
+            }
+
+            span {
+                color: #343434;
+                font-size: 1.31rem;
+            }
+
+        }/*fin estilo .card*/
+        
+    }/*fin estilo .carousel*/
+
+    
+    
+} /*fin estilo .contenedor*/
+
+
+
+
+@media screen and (max-width: 900px){
+    .contenedor {
+        .carousel { grid-auto-columns: calc((100% / 2) - 9px); }
+    }
+}
+
+@media screen and (max-width: 600px){
+    .contenedor {
+        .carousel  {grid-auto-columns: 100%; }
+        
+    }
+}
+
+                
+                <code></code></pre>`,
+                js: `<pre class="codigo"><code>
+                
+                
+const contenedor = document.querySelector(".contenedor");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".contenedor i");//llamo los botones flechas
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;//offsetWidth devuelve el ancho del layout del elemento, valor real del elemento
+const carouselChildrens = [...carousel.children]; //creando un array con los childrens de carousel
+
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// calcula el número de cards que se ven a la vez
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+/*Código para simular un scrollin infinito
+Para insertar copias al comienzo del carousel*/ 
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+/*Para insertar copias al final del carousel*/
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Agrego evento para arrow buttons para deslizar carousel hacia la izquiera o derecha
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth; //se traslada el ancho de una card a la izq o a la dcha
+    });
+});
+
+
+
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");//evita que el usuario pueda seleccionar el texto
+
+    // Guarda posición inicial del carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; // Si isDragging es falso 
+    // Para que el carousel se siga moviendo desde donde está a la izq automáticamente
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; 
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
+const infiniteScroll = () => {
+    // Si la galeria llega al comienzo, que continue con el final
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // Si la galería llega al final continua con el comiezo
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+    
+    // Se activa el autoplay si el mouse no está hover
+    clearTimeout(timeoutId);
+    if(!contenedor.matches(":hover")) autoPlay();
+}
+
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+contenedor.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+contenedor.addEventListener("mouseleave", autoPlay);
+
+
+                </code></pre>`,
+                objeto: `<pre class="codigo"><code>
+                import {slider3} from './main.js';
+                    
+                    ejemplo1: colocar un id
+                    new slider3(#id_del_padre) ;
+                        
+                    ejemplo2: colocar una clase
+                    new slider3(.class_del_padre) ;
+                        
+                    ejemplo3:colocar cualquier elemento html
+                    new slider3(div) ;
+                </code></pre>`
+        
             }
         ]
     
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import {crearLoader} from './objetosJs/Loader2.js';
 //import { librerias } from './libreria.js';
 import { CrearMenu, Item } from './objetosJs/menu1.js';
 import { CrearSlider } from './objetosJs/slider1.js';
+import { CrearSlider2 } from './objetosJs/slider2.js';
+import { CrearSlider3 } from './objetosJs/slider3.js';
 
 export class loader1 extends crearLoader{};
 
@@ -10,6 +12,10 @@ export class menu1 extends CrearMenu{};
 export class menuItem extends Item{};
 
 export class slider1 extends CrearSlider{};
+
+export class slider2 extends CrearSlider2{};
+
+export class slider3 extends CrearSlider3{};
 
 //new loader1('#ej-contenedor')
 
